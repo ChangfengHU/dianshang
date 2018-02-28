731451510279ee66cd45b703c47e6e8c82c0bc31
ChangfengHu <faker_322@outlook.com>
2018-03-01 02:44:38 +0800

    sku和颜色表多表联查,显示库存,接下俩进行更改库存

--- dianshang-dao/src/main/java/com/dianshang/core/dao/SkuDAO.java
+++ dianshang-dao/src/main/java/com/dianshang/core/dao/SkuDAO.java
@@ -3,11 +3,14 @@ package com.dianshang.core.dao;
 import com.dianshang.core.pojo.Sku;
 import com.github.abel533.mapper.Mapper;
 
+import java.util.List;
+
 /**
  * 库存管理DAO
  * @author Administrator
  *
  */
 public interface SkuDAO extends Mapper<Sku> {
+    List<Sku> findByProductId(String productId);
 
 }
--- dianshang-pojo/src/main/java/com/dianshang/core/pojo/Sku.java
+++ dianshang-pojo/src/main/java/com/dianshang/core/pojo/Sku.java
@@ -6,6 +6,16 @@ import java.util.Date;
 
 @Table(name="bbs_sku")
 public class Sku implements Serializable {
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
     /**
      * ID
      */
@@ -148,22 +158,18 @@ public class Sku implements Serializable {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", productId=").append(productId);
-        sb.append(", colorId=").append(colorId);
-        sb.append(", size=").append(size);
-        sb.append(", marketPrice=").append(marketPrice);
-        sb.append(", price=").append(price);
-        sb.append(", deliveFee=").append(deliveFee);
-        sb.append(", stock=").append(stock);
-        sb.append(", upperLimit=").append(upperLimit);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
+        return "Sku{" +
+                "color=" + color +
+                ", id=" + id +
+                ", productId=" + productId +
+                ", colorId=" + colorId +
+                ", size='" + size + '\'' +
+                ", marketPrice=" + marketPrice +
+                ", price=" + price +
+                ", deliveFee=" + deliveFee +
+                ", stock=" + stock +
+                ", upperLimit=" + upperLimit +
+                ", createTime=" + createTime +
+                '}';
     }
 }
